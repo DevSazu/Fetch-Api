93b64f8fc4174ba9225dfdc78b9626e11569555f
DevSazu <shazahansazu@gmail.com>
2026-08-12 02:01:07 +0600

    Es-6 partern

--- js/script.js
+++ js/script.js
@@ -1,20 +1,40 @@
-function onLoad(){
-    const url="https://jsonplaceholder.typicode.com/users";
+// function onLoad(){
+//     const url="https://jsonplaceholder.typicode.com/users";
+//     fetch(url)
+//     .then(res=>res.json())
+//     .then(data=>{
+//         showPost(data);
+//     });
+// }
+
+
+// function showPost(posts){
+//     const getData=document.getElementById('showdata');
+//     getData.innerHTML="";
+// posts.forEach(post => {
+//     const li= document.createElement('li');
+//     li.innerText=post.name;
+//     getData.appendChild(li);
+//     console.log(li)
+// });
+// }
+
+const onLoad=()=>{
+    const url = "https://jsonplaceholder.typicode.com/posts";
     fetch(url)
     .then(res=>res.json())
     .then(data=>{
-        showPost(data);
+        displayData(data)
     });
 }
 
-
-function showPost(posts){
-    const getData=document.getElementById('showdata');
-    getData.innerHTML="";
-posts.forEach(post => {
-    const li= document.createElement('li');
-    li.innerText=post.name;
-    getData.appendChild(li);
-    console.log(li)
-});
+const displayData=(posts)=>{
+ const getId= document.getElementById('showdata');
+ getId.innerHTML="";
+ 
+ posts.forEach(post => {
+    const li=document.createElement('li')
+    li.innerHTML=post.title;
+    getId.appendChild(li)
+ });
 }
