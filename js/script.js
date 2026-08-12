cc0ed41fe9fbd6187f3babdaa0fb8f9e85840f45
DevSazu <sazushazahan@gmail.com>
2026-08-12 22:41:32 +0600

    last pull

--- js/script.js
+++ js/script.js
@@ -37,4 +37,37 @@
 //     li.innerHTML=post.title;
 //     getId.appendChild(li)
 //  });
-// }
+// } 
+
+// design Ui
+const onLoad=()=>{
+    const url= "https://jsonplaceholder.typicode.com/posts";
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>{
+
+    displayData(data);
+    })
+}
+
+const displayData=(posts)=>{
+    const getId=document.getElementById('showdata');
+    getId.innerHTML="";
+    
+   posts.forEach(post=>{
+     const div=document.createElement('div');
+    div.className="card";
+     div.innerHTML=` 
+                <p>User ID: ${post.userId} </p>
+                <p>ID:${post.id} </p>
+
+                <h2>${post.title}</h2>
+
+                <p>${post.body} </p>
+
+                `
+            getId.appendChild(div);
+            console.log(div)
+           
+   })
+}
